03adbd52983e35cce7b205fe2ec12062fbc2b88f
DESKTOP-38CU0HO\Wexter <dantechomi@hotmail.com>
2018-04-12 17:54:00 -0500

    Mio

--- index2.php
+++ index2.php
@@ -63,29 +63,29 @@ $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
 
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb")== 0) {
-        continuar;
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
     }
     
-    $connectstr_dbhost = preg_replace ("/^.* Fuente de datos = (. +?);. * $ /", "\\ 1", $value);
-    $connectstr_dbname = preg_replace ("/^.* Database = (. +?);. * $ /", "\\ 1", $value);
-    $connectstr_dbusername = preg_replace ("/^.* User Id = (. +?);. * $ /", "\\ 1", $value);
-    $connectstr_dbpassword = preg_replace ("/^.* Contraseña = (. +?) $ /", "\\ 1", $valor);
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$link = mysqli_connect ($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
 if (!$link) {
-    echo "Error: no se puede conectar a MySQL". PHP_EOL;
-    echo "Depuración errno:". mysqli_connect_errno (). PHP_EOL;
-    echo "Error de depuración:". mysqli_connect_error (). PHP_EOL;
-    salida;
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 
-echo "Éxito: ¡se realizó una conexión adecuada con MySQL! La base de datos my_db es excelente". PHP_EOL;
-echo "Información del host:". mysqli_get_host_info ($link). PHP_EOL;
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close ($link);
+mysqli_close($link);
 ?>
 <br>
 <br>
