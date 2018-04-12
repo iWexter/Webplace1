227d7e4ffebacafb8b4dffe119145d636bec168a
DESKTOP-38CU0HO\Wexter <dantechomi@hotmail.com>
2018-04-12 17:27:43 -0500

    Mio

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "my_user";
+$password = "root";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
