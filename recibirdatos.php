955f0960dbf88aecffe3a083892bd24eea9a6a67
DESKTOP-38CU0HO\Wexter <dantechomi@hotmail.com>
2018-04-18 19:11:26 -0500

    Gooooasnldjahbsdk

--- recibirdatos.php
+++ recibirdatos.php
@@ -2,14 +2,35 @@
 
 $chipid =  $_POST["chipid"];
 $humedad = $_POST["humedad"];
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-$host = "localhost";
-$dbusername = "id5423092_rodrigo";
-$dbpassword = "quetzal90";
-$dbname = "id5423092_esp12";
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+
 
 // Create connection
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+$link = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
 if (mysqli_connect_error()){
   die('Connect Error ('. mysqli_connect_errno() .') '
@@ -18,14 +39,14 @@ if (mysqli_connect_error()){
 else{
   $sql = "INSERT INTO mediciones (chipid, humedad)
   values ('$chipid','$humedad')";
-  if ($conn->query($sql)){
+  if ($link->query($sql)){
     echo "New record is inserted sucessfully";
   }
   else{
     echo "Error: ". $sql ."
-". $conn->error;
+". $link->error;
   }
-  $conn->close();
+  $link->close();
 }
 
 
