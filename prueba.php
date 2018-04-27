52e259ffcc507246b3ea90551fc892fd6dc94d5b
DESKTOP-38CU0HO\Wexter <dantechomi@hotmail.com>
2018-04-26 19:38:02 -0500

    Go

--- prueba.php
+++ prueba.php
@@ -13,6 +13,62 @@
     FB.getLoginStatus(function(response) {
         console.log(response);
     });
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          FB.api(
+            '/me',
+            'GET', {
+              "fields": "accounts{name,access_token,id}"
+            },
+            function(response) {
+              console.log(response.accounts.data);
+              console.log(response.accounts.data.id);
+              FB.api(
+                '/' + idp,
+                'GET', {
+                  "fields": "id,access_token"
+                },
+                function(response) {
+                  console.log(response);
+                  console.log(response.access_token);
+                  var at = response.access_token;
+                  FB.api(
+                    '/344806002657871/photos',
+                    'POST', {
+                      "message": "Churros :v",
+                      "link": "https://www.rubios.com/sites/default/files/styles/menu_item_cropped/public/menu/churros.jpg?itok=GWFxLVTM",
+                      "access_token": at
+                    },
+                    function(response) {
+                      console.log(response);
+                    }
+                  );
+                  // FB.api(
+                  //   '/' + idp + '/feed',
+                  //   'POST', {
+                  //     "message": msg,
+                  //     "link": link,
+                  //     "access_token": at,
+                  //     "fields": "created_time,from,id,message,permalink_url"
+                  //   },
+                  //   function(response) {
+                  //     console.log(response);
+                  //   }
+                  // );
+
+                }
+              );
+            }
+          );
+
+        } else {
+          FB.login(function(response) {
+            // handle the response
+          }, {
+            scope: 'manage_pages,pages_show_list,publish_pages'
+          });
+        }
+    });
       
   };
 
